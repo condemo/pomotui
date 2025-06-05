afa7ab3bab5c0e231e28fafb830747f747d8791e
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-06-05 23:45:11 +0200

    feat: `HomeView.timerProgress` widget

--- views/home.go
+++ views/home.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/charmbracelet/bubbles/v2/help"
 	"github.com/charmbracelet/bubbles/v2/key"
+	"github.com/charmbracelet/bubbles/v2/progress"
 	"github.com/charmbracelet/bubbles/v2/timer"
 	tea "github.com/charmbracelet/bubbletea/v2"
 	"github.com/condemo/pomotui/keymaps"
@@ -14,9 +15,11 @@ import (
 )
 
 // TODO: Crear un módulo de configuración para hacer dinámico todo esto
-const timeout = time.Minute * 30
-const shortTimeout = time.Minute * 5
-const longTimeout = time.Minute * 15
+const (
+	timeout      = time.Minute * 30
+	shortTimeout = time.Minute * 5
+	longTimeout  = time.Minute * 15
+)
 
 type timerMode string
 
@@ -26,11 +29,18 @@ const (
 	longBreak  timerMode = "Long Break"
 )
 
+// TODO: debería ser todo dinámico
+const (
+	incPercent = .0005555
+	maxWidth   = 25
+)
+
 type HomeView struct {
-	keys      keymaps.HomeKeyMap
-	timerMode timerMode
-	help      help.Model
-	timer     timer.Model
+	keys          keymaps.HomeKeyMap
+	timerMode     timerMode
+	help          help.Model
+	timer         timer.Model
+	timerProgress progress.Model
 }
 
 func NewHomeView() HomeView {
@@ -38,7 +48,11 @@ func NewHomeView() HomeView {
 		keys:      keymaps.NewHomeKeyMap(),
 		help:      help.New(),
 		timerMode: work,
-		timer:     timer.New(timeout, timer.WithInterval(time.Millisecond)),
+		timer:     timer.New(timeout, timer.WithInterval(time.Second)),
+		timerProgress: progress.New(
+			progress.WithoutPercentage(),
+			progress.WithDefaultGradient(),
+		),
 	}
 }
 
@@ -52,8 +66,10 @@ func (m HomeView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case timer.TickMsg:
+		var pc tea.Cmd
 		m.timer, cmd = m.timer.Update(msg)
-		return m, cmd
+		pc = m.timerProgress.IncrPercent(incPercent)
+		return m, tea.Batch(cmd, pc)
 	case timer.StartStopMsg:
 		m.timer, cmd = m.timer.Update(msg)
 		m.keys.Pause.SetEnabled(m.timer.Running())
@@ -64,9 +80,21 @@ func (m HomeView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, m.timer.Toggle()
 		case key.Matches(msg, m.keys.Reset):
 			m.timer.Timeout = timeout
+			return m, m.timerProgress.SetPercent(0)
 		case key.Matches(msg, m.keys.Help):
 			// TODO: Reactivar `FullHelp` cuando haga falta
 		}
+	case tea.WindowSizeMsg:
+		m.timerProgress.SetWidth(msg.Width / 3)
+		if m.timerProgress.Width() > maxWidth {
+			m.timerProgress.SetWidth(maxWidth)
+		}
+		return m, nil
+
+	case progress.FrameMsg:
+		m.timerProgress, cmd = m.timerProgress.Update(msg)
+		return m, cmd
+
 	case messages.ViewChanged:
 		return m, m.timer.Stop()
 	}
@@ -75,9 +103,11 @@ func (m HomeView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m HomeView) View() string {
 	mode := fmt.Sprintf("[ %s ]", string(m.timerMode))
+	pb := m.timerProgress.View()
 
 	return strings.Repeat("\n", 2) +
-		"\t" + m.timer.View() +
 		"\n\t" + mode +
+		"\n\t" + m.timer.View() +
+		"\n\t" + pb +
 		strings.Repeat("\n", 3) + m.help.View(m.keys)
 }
