922a64d2623f63e81e790443d2ce7966d4439ff4
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-06-10 23:30:41 +0200

    feat: timers form v1

--- views/config.go
+++ views/config.go
@@ -14,30 +14,55 @@ type ConfigView struct {
 	form *huh.Form
 }
 
+var confirmed bool
+
 func NewConfig() ConfigView {
 	return ConfigView{
 		keys: keymaps.NewConfigKeyMap(),
 		form: huh.NewForm(
 			huh.NewGroup(
 				huh.NewSelect[time.Duration]().
-					Title("Work Timer").
+					Title("Work").
 					Options(
-						huh.NewOption("20 minutes", time.Minute*20),
-						huh.NewOption("25 minutes", time.Minute*25),
-						huh.NewOption("30 minutes", time.Minute*30),
-						huh.NewOption("35 minutes", time.Minute*35),
+						huh.NewOption("20m", time.Minute*20),
+						huh.NewOption("25m", time.Minute*25),
+						huh.NewOption("30m", time.Minute*30),
+						huh.NewOption("35m", time.Minute*35),
 					).Key("work"),
-			),
+				huh.NewSelect[time.Duration]().
+					Title("Short Break").
+					Options(
+						huh.NewOption("3m", time.Minute*3),
+						huh.NewOption("4m", time.Minute*4),
+						huh.NewOption("5m", time.Minute*5),
+						huh.NewOption("6m", time.Minute*6),
+					).Key("short"),
+				huh.NewSelect[time.Duration]().
+					Title("Long Break").
+					Options(
+						huh.NewOption("10m", time.Minute*10),
+						huh.NewOption("15m", time.Minute*15),
+						huh.NewOption("20m", time.Minute*20),
+						huh.NewOption("25m", time.Minute*25),
+					).Key("long"),
+				huh.NewConfirm().
+					Title("Ara you sure?").Affirmative("yes!").Negative("no.").
+					Value(&confirmed),
+			).WithTheme(huh.ThemeCatppuccin()),
 		),
 	}
 }
 
 func (m ConfigView) Init() tea.Cmd {
-	return nil
+	return m.form.Init()
 }
 
 func (m ConfigView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+	form, cmd := m.form.Update(msg)
+	if f, ok := form.(*huh.Form); ok {
+		m.form = f
+	}
 
 	return m, cmd
 }
